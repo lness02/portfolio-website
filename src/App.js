5db9942a686c35fd08bfd10ff66b8926dcdf3dbf
Laura Ness <lness@ucla.edu>
2025-02-10 21:23:50 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
